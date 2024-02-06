ac3bc4969a1ce7f1ce689e034c95af06c090996d
Pmi404 <multipedrolucas@gmail.com>
2024-02-06 17:48:01 -0300

    All Questions created

--- script.js
+++ script.js
@@ -96,6 +96,31 @@ const perguntas = [
   console.log(perguntas[0].respostas);
   console.log(perguntas[0].correta);
 
-  
+  const quiz = document.querySelector("#quiz")
+  const template = document.querySelector('template')
+
+
+  // Loop ou laço de repetição
+  for(const item of perguntas) {
+    const quizItem = template.content.cloneNode(true)
+    quizItem.querySelector('h3').textContent = item.pergunta
+    
+    // Faz com que a 'Resposta A' se clone ao total de repostas de cada pergunta 
+    //e crie cada resposta com seu perspectivo texto
+    for(let resposta of item.respostas) {
+      const dt = quizItem.querySelector('dl dt').cloneNode(true)
+      dt.querySelector("span").textContent = resposta
+
+
+      quizItem.querySelector('dl').appendChild(dt)
+
+    }
+
+    // Remove o 'Resposta A'
+    quizItem.querySelector('dl dt').remove()
+
+    // coloca a pergunta na tela
+    quiz.appendChild(quizItem)
+  }
   
 
